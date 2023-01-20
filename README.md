4add7070c10239e6e262d62f97b691d7de9e805c
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-01-19 21:46:15 -0500

    finish java version (needs testing)

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # highlightjs-lang-colorized
+
 A highlight.js grammar and css ruleset which colorizes Minecraft's lang code such that the values match what it would look like in-game. Supports Java and Bedrock edition's different rendering rules.
 
 > **Note**: This repository is not done yet, but when it is this warning will be removed.
+
+## Usage
+
+### Browser
+
+To use on your webpage, link both the compiled script and the style.
+You will also need the base highlight.js library.
+
+```html
+<script src="https://adamraichu.github.io/highlightjs-lang-colorized/dist/lang-colorized-java.min.js"></script>
+<script src="https://adamraichu.github.io/highlightjs-lang-colorized/dist/lang-colorized-bedrock.min.js"></script>
+<!-- Note: If you only plan on highlighting one flavor of syntax, there is no need to include both here. -->
+<link rel="stylesheet" href="https://adamraichu.github.io/highlightjs-lang-colorized/src/styles/lang-colorized.css">
+```
+
+To highlight a code block, give it the class `language-lang-colorized-<version>`.
+Then call `hljs.highlightAll()`.
+*The version you choose will affect how certain `§` formatters and highlighters take effect.*
--- src/languages/lang-colorized-java.js
+++ src/languages/lang-colorized-java.js
@@ -5,11 +5,48 @@
 */
 
 module.exports = function (hljs) {
+  var selector_node = {
+    className: "lang-selector",
+    begin: "§(?=[0-9a-gk-or]{1})",
+    end: "(?<=§)[0-9a-gk-or]{1}",
+  };
+  var format_selectors = [
+    selector_node,
+    {
+      className: "lang-format-k",
+      begin: "§k",
+      end: "(?=§[0-9a-fr])|(?!.)",
+      contains: [selector_node],
+    },
+    {
+      className: "lang-format-l",
+      begin: "§l",
+      end: "(?=§[0-9a-fr])|(?!.)",
+      contains: [selector_node],
+    },
+    {
+      className: "lang-format-m",
+      begin: "§m",
+      end: "(?=§[0-9a-fr])|(?!.)",
+      contains: [selector_node],
+    },
+    {
+      className: "lang-format-n",
+      begin: "§n",
+      end: "(?=§[0-9a-fr])|(?!.)",
+      contains: [selector_node],
+    },
+    {
+      className: "lang-format-o",
+      begin: "§o",
+      end: "(?=§[0-9a-fr])|(?!.)",
+      contains: [selector_node],
+    },
+  ];
   return {
     name: "lang-colorized-java",
     disableAutodetect: true,
     case_insensitive: false,
-    // §[0-9a-gk-or]{1}
     contains: [
       {
         className: "attr",
@@ -24,14 +61,106 @@ module.exports = function (hljs) {
       },
       {
         className: "lang-java",
-        begin: "(?<=\\=)[A-Za-z]",
+        begin: "(?<==).",
         end: "(?!.)|(?=#)",
         contains: [
           {
             className: "lang-format-0",
             begin: "§0",
             end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
           },
+          {
+            className: "lang-format-1",
+            begin: "§1",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-2",
+            begin: "§2",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-3",
+            begin: "§3",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-4",
+            begin: "§4",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-5",
+            begin: "§5",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-6",
+            begin: "§6",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-7",
+            begin: "§7",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-8",
+            begin: "§8",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-9",
+            begin: "§9",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-a",
+            begin: "§a",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-b",
+            begin: "§b",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-c",
+            begin: "§c",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-d",
+            begin: "§d",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-e",
+            begin: "§e",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          {
+            className: "lang-format-f",
+            begin: "§f",
+            end: "(?=§[0-9a-fr])|(?!.)",
+            contains: format_selectors,
+          },
+          // Java doesn't have §g
         ],
       },
       hljs.COMMENT("#", "$"),
